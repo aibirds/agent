7fd4c6297900a0c818243e8d3ee4352ca9b09195
AIBirds.org <angrybirdscompetition@gmail.com>
2013-04-05 12:41:59 +1200

    license updated

--- plugin/script.js
+++ plugin/script.js
@@ -1,11 +1,11 @@
 /*****************************************************************************
 ** ANGRYBIRDS AI AGENT FRAMEWORK
-** Copyright (c) 2013, Sahan Abeyasinghe, XiaoYu (Gary) Ge, Stephen Gould,
-** Jim Keys, Kar-Wai Lim, Jochen Renz
+** Copyright (c) 2013, XiaoYu (Gary) Ge, Stephen Gould, Jochen Renz
+**  Sahan Abeyasinghe,Jim Keys, Kar-Wai Lim, Zain Mubashir, Andrew Wang, Peng Zhang
 ** All rights reserved.
-**
-** This software is distributed under terms of the BSD license. See the 
-** LICENSE file in the root directory for details.
+**This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
+**To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
+*or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 *****************************************************************************/
 
 (function() {
